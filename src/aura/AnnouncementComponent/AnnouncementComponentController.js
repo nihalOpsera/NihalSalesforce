fb117d3e52f96bdf27ab67238cafb5f93cadce49
Nihal Sekhar <85214474+nihalOpsera@users.noreply.github.com>
2022-03-31 12:26:49 +0530

    Delete src directory

--- src/aura/AnnouncementComponent/AnnouncementComponentController.js
+++
@@ -1,9 +0,0 @@
-({
-	myAction : function(component, event, helper) {
-        var mActionGetMappedValues = component.get("c.getAnnouncements");
-		mActionGetMappedValues.setCallback(this, function(response) {
-        	component.set("v.announcements", response.getReturnValue());
-        }); 
-        $A.enqueueAction(mActionGetMappedValues);
-	}
-})
