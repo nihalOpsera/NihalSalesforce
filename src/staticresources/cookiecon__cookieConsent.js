776a79f74142d27f1b66e1c77ac3a027ac58a008
nihalOpsera <nihalOpsera>
2022-12-07 16:02:44 +0000

    Committing selected files as part of Opsera Bulk retrieval task - 62737000cb3f09001b568ce9, run Count - 16.. Commit Number-68

---
+++ src/staticresources/cookiecon__cookieConsent.js
@@ -0,0 +1,21 @@
+/**
+ * Due to locker service restrictions, we can not access a documents cookies within a component. We can include this script in our
+ * communities head to serve as a broker for getting cookies from the dom. we cna fire the 'componentConnected' custom event and
+ * capture it in this script. We can then get the cookies from the document and create the documentCookies custom event and fire it.
+ * This event can be captured by the component for processing.
+ */
+window.addEventListener("componentConnected", function(e) {
+  let cookies = document.cookie;
+  let event = new CustomEvent("documentCookies", { detail: cookies });
+  window.dispatchEvent(event);
+});
+
+window.addEventListener("deleteCookies", function(e) {
+  let cookies = e.detail;
+  for (let i = 0, len = cookies.length; i < len; i++) {
+    let cookieWithStandardPath = cookies[i] + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;";
+    let cookieWithCommunityPath = cookies[i] + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/s;";
+    document.cookie = cookieWithStandardPath;
+    document.cookie = cookieWithCommunityPath;
+  }
+});
